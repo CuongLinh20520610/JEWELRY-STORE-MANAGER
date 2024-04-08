246a62d6d01e95c3d1688fb03afad78869701b7f
MTien2901 <105910698+MTien2901@users.noreply.github.com>
2024-04-08 08:54:10 +0700

    Delete SE104 directory

--- SE104/SE104/Models/PhieuBanHang.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SE104.Models
-{
-    public class PhieuBanHang
-    {
-        public String SoPhieu { get; set; } = string.Empty;
-        public DateTime NgayLap { get; set; }
-        public String MaKH { get; set; } = string.Empty;
-    }
-}
